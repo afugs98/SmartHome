1794a1cae1f9be2db82a9d48377311d61592c84d
Alec Fuglem <afuglem@MBP.local>
2018-12-01 20:51:09 -0500

    Gets the outlet up and running, converts over the auto commisionl, is now an outlet, pairs, stable

--- Devices/Commisioned_Outlet/main.c
+++ Devices/Commisioned_Outlet/main.c
@@ -23,23 +23,23 @@
 #include <task.h>
 #include <math.h>
 
-
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
 #include <wifi_config.h>
 
 const int led_gpio = 2;
+void led_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context);
 
 void led_write(bool on)
 {
    gpio_write(led_gpio, on ? 0 : 1);
 }
 
-void led_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context);
-
 homekit_characteristic_t led_on = HOMEKIT_CHARACTERISTIC_(
    ON, false, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(led_on_callback)
 );
+
+bool outletStatus = false;
 
 void led_init()
 {
@@ -80,34 +80,54 @@ void led_identify(homekit_value_t _value)
 
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "Outlet safety");
 
-homekit_accessory_t *accessories[] =
-{
-   HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
-      HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
-      &name,
-      HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HaPK"),
-      HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "037A2BABF19D"),
-      HOMEKIT_CHARACTERISTIC(MODEL, "MyLED"),
-      HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
-      HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify),
-      NULL
-      }),
-      HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]){
-      HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
-      &led_on,
-      NULL
-      }),
-      NULL
-      }),
-   NULL
-};
+homekit_characteristic_t switch_outlet_in_use = HOMEKIT_CHARACTERISTIC_(OUTLET_IN_USE, true);
 
+homekit_value_t led_on_get() {
+    return HOMEKIT_BOOL(outletStatus);
+}
+
+void led_on_set(homekit_value_t value) {
+    if (value.format != homekit_format_bool) {
+        printf("Invalid value format: %d\n", value.format);
+        return;
+    }
+
+    outletStatus = value.bool_value;
+    led_write(outletStatus);
+}
+
+homekit_accessory_t *accessories[] =
+   {
+      HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_outlet, .services=(homekit_service_t*[]){
+         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+         &name,
+         HOMEKIT_CHARACTERISTIC(MANUFACTURER, "TrapHouse"),
+         HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "137A3BRCF11A"),
+         HOMEKIT_CHARACTERISTIC(MODEL, "OutletGen1"),
+         HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "4.20"),
+         HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify),
+         NULL
+         }),
+         HOMEKIT_SERVICE(OUTLET, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+         HOMEKIT_CHARACTERISTIC(NAME, "Outlet"),
+         &switch_outlet_in_use,
+         HOMEKIT_CHARACTERISTIC(
+         ON, false,
+         .getter=led_on_get,
+         .setter=led_on_set
+         ),
+         NULL
+         }),
+         NULL
+         }),
+      NULL
+   };
 
 homekit_server_config_t config =
-{
-   .accessories = accessories,
-   .password = "111-11-111"
-};
+   {
+      .accessories = accessories,
+      .password = "111-11-111"
+   };
 
 void on_wifi_ready()
 {
@@ -121,11 +141,11 @@ void user_init(void)
    // accessory name suffix.
    uint8_t macaddr[6];
    sdk_wifi_get_macaddr(STATION_IF, macaddr);
-   int name_len = snprintf(NULL, 0, "Sample LED Strip-%02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
+   int name_len = snprintf(NULL, 0, "Outlet-%02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
    char *name_value = malloc(name_len + 1);
-   snprintf(name_value, name_len + 1, "Sample LED Strip-%02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
+   snprintf(name_value, name_len + 1, "Outlet-%02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
    name.value = HOMEKIT_STRING(name_value);
 
-   wifi_config_init("my-accessory", NULL, on_wifi_ready);
+   wifi_config_init("TrapHouse:", NULL, on_wifi_ready);
    led_init();
 }

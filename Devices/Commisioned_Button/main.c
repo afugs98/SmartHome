699436dd183bd8e869a85d222d04e6f5bb48ae73
Alec Fuglem <afuglem@MBP.kaos>
2018-12-24 12:23:07 -0600

    Adds in the comissioning, makes it much easier to use

--- Devices/Commisioned_Button/main.c
+++ Devices/Commisioned_Button/main.c
@@ -8,6 +8,8 @@
 
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
+#include <wifi_config.h>
+
 #include "button.h"
 #include "wifi.h"
 
@@ -15,18 +17,18 @@
 #error BUTTON_PIN is not specified
 #endif
 
-static void wifi_init()
-{
-   struct sdk_station_config wifi_config =
-   {
-      .ssid = WIFI_SSID,
-      .password = WIFI_PASSWORD,
-   };
-
-   sdk_wifi_set_opmode(STATION_MODE);
-   sdk_wifi_station_set_config(&wifi_config);
-   sdk_wifi_station_connect();
-}
+//static void wifi_init()
+//{
+//   struct sdk_station_config wifi_config =
+//   {
+//      .ssid = WIFI_SSID,
+//      .password = WIFI_PASSWORD,
+//   };
+//
+//   sdk_wifi_set_opmode(STATION_MODE);
+//   sdk_wifi_station_set_config(&wifi_config);
+//   sdk_wifi_station_connect();
+//}
 
 void button_identify(homekit_value_t _value)
 {
@@ -56,54 +58,89 @@ void button_callback(uint8_t gpio, button_event_t event)
    }
 }
 
+homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "Button");
+
 homekit_accessory_t *accessories[] =
-{
-   HOMEKIT_ACCESSORY(
-      .id = 1,
-      .category = homekit_accessory_category_programmable_switch,
-      .services=(homekit_service_t*[]) {
-      HOMEKIT_SERVICE(
-      ACCESSORY_INFORMATION,
-      .characteristics=(homekit_characteristic_t*[]) {
-      HOMEKIT_CHARACTERISTIC(NAME, "Button"),
-      HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HaPK"),
-      HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0012345"),
-      HOMEKIT_CHARACTERISTIC(MODEL, "MyButton"),
-      HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
-      HOMEKIT_CHARACTERISTIC(IDENTIFY, button_identify),
+   {
+      HOMEKIT_ACCESSORY(
+         .id = 1,
+         .category = homekit_accessory_category_programmable_switch,
+         .services=(homekit_service_t*[]) {
+         HOMEKIT_SERVICE(
+         ACCESSORY_INFORMATION,
+         .characteristics=(homekit_characteristic_t*[]) {
+         &name,
+         HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HaPK"),
+         HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0012345"),
+         HOMEKIT_CHARACTERISTIC(MODEL, "MyButton"),
+         HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+         HOMEKIT_CHARACTERISTIC(IDENTIFY, button_identify),
+         NULL
+         },
+         ),
+         HOMEKIT_SERVICE(
+         STATELESS_PROGRAMMABLE_SWITCH,
+         .primary=true,
+         .characteristics=(homekit_characteristic_t*[]) {
+         HOMEKIT_CHARACTERISTIC(NAME, "Button"),
+         &button_event,
+         NULL
+         },
+         ),
+         NULL
+         },
+         ),
       NULL
-      },
-      ),
-      HOMEKIT_SERVICE(
-      STATELESS_PROGRAMMABLE_SWITCH,
-      .primary=true,
-      .characteristics=(homekit_characteristic_t*[]) {
-      HOMEKIT_CHARACTERISTIC(NAME, "Button"),
-      &button_event,
-      NULL
-      },
-      ),
-      NULL
-      },
-      ),
-   NULL
-};
+   };
 
 homekit_server_config_t config =
+   {
+      .accessories = accessories,
+      .password = "111-11-111"
+   };
+
+void on_wifi_ready()
 {
-   .accessories = accessories,
-   .password = "111-11-111"
-};
+   homekit_server_init(&config);
+}
+
+void create_accessory_name()
+{
+   uint8_t macaddr[6];
+   sdk_wifi_get_macaddr(STATION_IF, macaddr);
+
+   int name_len = snprintf(NULL, 0, "Button-%02X%02X%02X",
+      macaddr[3], macaddr[4], macaddr[5]);
+   char *name_value = malloc(name_len + 1);
+   snprintf(name_value, name_len + 1, "Button-%02X%02X%02X",
+      macaddr[3], macaddr[4], macaddr[5]);
+
+   name.value = HOMEKIT_STRING(name_value);
+}
 
 void user_init(void)
 {
    uart_set_baud(0, 115200);
 
-   wifi_init();
+   create_accessory_name();
+
+   wifi_config_init("TrapHouse_Button", NULL, on_wifi_ready);
+
    if(button_create(BUTTON_PIN, button_callback))
    {
       printf("Failed to initialize button\n");
    }
-   homekit_server_init(&config);
 }
+
+//void user_init(void)
+//{
+//   uart_set_baud(0, 115200);
+//
+//   wifi_init();
+//   if(button_create(BUTTON_PIN, button_callback))
+//   {
+//      printf("Failed to initialize button\n");
+//   }
+//   homekit_server_init(&config);
+//}
 
